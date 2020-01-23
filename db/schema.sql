e8ba8f646199d06aa6e59cd33553ac348ececf92
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-23 05:15:59 -0500

    please don't add node modules

--- db/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
-CREATE TABLE burgers(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255),
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY(id)
-)
--- db/seeds.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO burgers(burger_name) VALUES("The Pounder"), ("Big Mac"), ("Whopper")
